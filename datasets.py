ffb679d3b76650554dd87b88cd2d8716321ded0d
Mehdi AIT ARYANE <mehdi.aitaryane@outlook.com>
2023-12-27 00:17:40 +0100

    count occurances

--- datasets.py
+++ datasets.py
@@ -4,6 +4,11 @@ import numpy as np
 def random_init(size_in, size_out):
     return np.random.uniform(0.0, 1.0, size=(size_in, size_out))
 
+def count_occurances(y):
+    arr = np.unique(y, return_counts=True)
+    for i in range(len(arr[0])):
+        print("class ", arr[0][i], " has ", arr[1][i], " items")
+
 def make_classification(n_samples=101, n_features=20, n_classes=2, noise=0.0, balanced=True, random_state=None, init = random_init):
     # Select random state
     np.random.seed(random_state)

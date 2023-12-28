3368c2482b8e2fafd3cce916697eb622cf94f14a
Mehdi AIT ARYANE <mehdi.aitaryane@outlook.com>
2023-12-28 01:59:16 +0100

    documentation for plots module

--- plots.py
+++ plots.py
@@ -1,11 +1,30 @@
 import matplotlib.pyplot as plt
 
+# This function creates a 2D scatter plot of the input data.
+# Parameters:
+# title: str, Title of the plot
+# xlabel: str, Label for the x-axis
+# ylabel: str, Label for the y-axis
+# X: np.ndarray, Input array of features
+# y: np.ndarray, Input array of target values
+# Returns: None
+
 def scatter2D(title, xlabel, ylabel, X, y):
     plt.scatter(X[:, 0], X[:, 1], c = y)
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.show()
+
+# This function creates a 3D scatter plot of the input data.
+# Parameters:
+# title: str, Title of the plot
+# xlabel: str, Label for the x-axis
+# ylabel: str, Label for the y-axis
+# zlabel: str, Label for the z-axis
+# X: np.ndarray, Input array of features
+# y: np.ndarray, Input array of target values
+# Returns: None
 
 def scatter3D(title, xlabel, ylabel, zlabel, X, y):
     # Creating plot
